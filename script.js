69663dd4cf6691e0accff59fce6132de83ff9d0d
Yamisa <majijayamisa@gmail.com>
2022-08-29 13:39:35 +0200

    update

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ document.querySelector('#btnAdd').onclick = function(){
         <div class="Tasks">
             <span id="TaskName">
                 ${document.querySelector('#addTask input').value}</span>
-                <button class="delete">DELETE
+                <button class="delete"><img src="delete.png">
                 </button>
-        `
+                <hr>
+            
+        `;
+        document.querySelector('#addTask input').value="";
+    } var current = document.querySelectorAll(".delete");
+    for( var i=0; i<current.length;i++){
+        current[i].onclick= function(){
+            this.parentNode.remove()
+        }
     }
 }
